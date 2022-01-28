0472f04e9b3fc37d627530f1ea8323e42d8946e1
Ramzan Paragulgov <61330017+paragulgov@users.noreply.github.com>
2022-01-28 23:02:04 +0300

    Swagger for tournaments

--- src/tournaments/dto/create-tournament.dto.ts
+++ src/tournaments/dto/create-tournament.dto.ts
@@ -1,6 +1,8 @@
 import { IsNotEmpty, IsDefined } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTournamentDto {
+  @ApiProperty()
   @IsDefined({ message: 'Укажите название турнира' })
   @IsNotEmpty({ message: 'Укажите название турнира' })
   name: string;
--- src/tournaments/entities/tournament.entity.ts
+++ src/tournaments/entities/tournament.entity.ts
@@ -5,18 +5,23 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('tournaments')
 export class TournamentEntity {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty()
   @Column({ nullable: false })
   name: string;
 
+  @ApiProperty()
   @CreateDateColumn({ type: 'timestamp' })
   createdAt: Date;
 
+  @ApiProperty()
   @UpdateDateColumn({ type: 'timestamp' })
   updatedAt: Date;
 }
