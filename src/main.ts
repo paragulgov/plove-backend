cb45281ac53d0b793476dfea8e19d7b5dc9e8857
Ramzan Paragulgov <61330017+paragulgov@users.noreply.github.com>
2022-02-18 21:08:28 +0300

    deploy

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
 
