b7129b1a84d07752d545792a1f3230247b46add3
Ramzan Paragulgov <61330017+paragulgov@users.noreply.github.com>
2022-02-18 20:01:43 +0300

    deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,24 +3,38 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TournamentsModule } from './tournaments/tournaments.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TournamentEntity } from './tournaments/entities/tournament.entity';
 import { MatchesModule } from './matches/matches.module';
+import { MatchEntity } from './matches/entities/match.entity';
 import { UsersModule } from './users/users.module';
+import { UserEntity } from './users/entities/user.entity';
 import { BetsModule } from './bets/bets.module';
+import { BetEntity } from './bets/entities/bet.entity';
 import { StatisticsModule } from './statistics/statistics.module';
+import { StatisticEntity } from './statistics/entities/statistic.entity';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseConfig } from './database.config';
-import { config } from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [config],
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useClass: DatabaseConfig,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_URL,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [
+        TournamentEntity,
+        MatchEntity,
+        UserEntity,
+        BetEntity,
+        StatisticEntity,
+      ],
+      synchronize: true,
     }),
     TournamentsModule,
     MatchesModule,
